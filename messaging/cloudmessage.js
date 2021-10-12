7e5e1134f5d05464f0c027f2301888c252de47a7
Dharmesh Upadhyay <udharmesh61@gmail.com>
2021-10-12 18:50:03 +0530

    see

--- messaging/cloudmessage.js
+++ messaging/cloudmessage.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require('./seviceAccountKey');
+const serviceAccount = require('./seviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(  serviceAccount)
